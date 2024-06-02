2894c0dbd3fa4941cd6f5297e85c78a9811ab4d5
Sergey Shulga <shul.sergio@gmail.com>
2024-06-02 13:38:41 +0300

    new add less
    add lessons

--- js/js-lessons.js
+++ js/js-lessons.js
@@ -866,3 +866,165 @@ function inp(a, b) {
   return result;
 }
 console.log(inp(20, 10));
+
+//Напишіть дві функції
+// letMeSeeYourName(callback) - запитує ім'я користувача
+//через prompt та викликає callback функцію
+//greet(name) - коллбек, що приймає ім'я і логірує в консоль
+//Рядок "Привіт <name>"
+
+// function letMeSeeYourName(callback) {
+//   const userName = prompt("введіть ім*я");
+//   if (!userName) {
+//     return alert("ви не ввели ім*я");
+//   }
+//   callback(userName);
+// }
+
+// function greet(name) {
+//   console.log(`Привіт ${name}`);
+// }
+// letMeSeeYourName(greet);
+
+//Напишіть дві функції
+//makeProduct(name, price, callback) - приймає
+//ім'я та ціну товару, а також callback.
+//Функція створює об'єкт товару, додаючи йому унікальний
+//ідентифікатор як id і викликає callback
+//Передаючи йому створений об'єкт.
+//showProduct(product) - коллбек приймаючий об'єкт
+//продукту і логірующий їх у консоль
+
+// function makeProduct(name, price, callback) {
+//   const product = {
+//     name,
+//     price,
+//     };
+//     callback(product);
+// }
+
+// showProduct(product){
+//     console.log(product);
+// };
+
+// makeProduct("Samsung", 100500, showProduct);
+
+// const users = [
+//   {
+//     id: "701b29c3-b35d-4cf1-a5f6-8b12b29a5081",
+//     name: "Moore Hensley",
+//     email: "moorehensley@indexia.com",
+//     eyeColor: "blue",
+//     friends: ["Sharron Pace"],
+//     isActive: false,
+//     balance: 2811,
+//     skills: ["ipsum", "lorem"],
+//     gender: "male",
+//     age: 37,
+//   },
+//   {
+//     id: "7a3cbd18-57a1-4534-8e12-1caad921bda1",
+//     name: "Sharlene Bush",
+//     email: "sharlenebush@tubesys.com",
+//     eyeColor: "blue",
+//     friends: ["Briana Decker", "Sharron Pace"],
+//     isActive: true,
+//     balance: 3821,
+//     skills: ["tempor", "mollit", "commodo", "veniam", "laborum"],
+//     gender: "female",
+//     age: 34,
+//   },
+//   {
+//     id: "88beb2f3-e4c2-49f3-a0a0-ecf957a95af3",
+//     name: "Ross Vazquez",
+//     email: "rossvazquez@xinware.com",
+//     eyeColor: "green",
+//     friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
+//     isActive: false,
+//     balance: 3793,
+//     skills: ["nulla", "anim", "proident", "ipsum", "elit"],
+//     gender: "male",
+//     age: 24,
+//   },
+//   {
+//     id: "249b6175-5c30-44c6-b154-f120923736f5",
+//     name: "Elma Head",
+//     email: "elmahead@omatom.com",
+//     eyeColor: "green",
+//     friends: ["Goldie Gentry", "Aisha Tran"],
+//     isActive: true,
+//     balance: 2278,
+//     skills: ["adipisicing", "irure", "velit"],
+//     gender: "female",
+//     age: 21,
+//   },
+//   {
+//     id: "334f8cb3-eb04-45e6-abf4-4935dd439b70",
+//     name: "Carey Barr",
+//     email: "careybarr@nurali.com",
+//     eyeColor: "blue",
+//     friends: ["Jordan Sampson", "Eddie Strong"],
+//     isActive: false,
+//     balance: 3951,
+//     skills: ["ex", "culpa", "nostrud"],
+//     gender: "male",
+//     age: 27,
+//   },
+//   {
+//     id: "150b00fb-dd82-427d-9faf-2879ea87c695",
+//     name: "Blackburn Dotson",
+//     email: "blackburndotson@furnigeer.com",
+//     eyeColor: "brown",
+//     friends: ["Jacklyn Lucas", "Linda Chapman"],
+//     isActive: false,
+//     balance: 1498,
+//     skills: ["non", "amet", "ipsum"],
+//     gender: "male",
+//     age: 38,
+//   },
+//   {
+//     id: "e1bf46ab-7168-491e-925e-f01e21394812",
+//     name: "Sheree Anthony",
+//     email: "shereeanthony@kog.com",
+//     eyeColor: "brown",
+//     friends: ["Goldie Gentry", "Briana Decker"],
+//     isActive: true,
+//     balance: 2764,
+//     skills: ["lorem", "veniam", "culpa"],
+//     gender: "female",
+//     age: 39,
+//   },
+// ];
+// Отримати масив імен всіх користувачів (поле name).
+
+// const getUserNames = (users) => {
+//   return users.map((user) => user.name);
+// };
+
+// console.log(getUserNames(users));
+
+// Отримати масив об'єктів користувачів за кольором очей (поле eyeColor) та за статтю (поле gender)
+
+// const getUserSorted = (users, eyeColor, gender) => {
+//   return users.filter(
+//     (user) => user.eyeColor === eyeColor && user.gender === gender
+//   );
+// };
+
+// console.log(getUserSorted(users, "brown", "female"));
+
+// Отримати масив імен тільки неактивних користувачів віком від min до max і які мають зазначеного друга
+
+// const getNamesInactiveUsersWithFriend = (users, age, friend) => {
+//   return users.reduce((acc, user) => {
+//     const isTrue =
+//       user.isActive === false &&
+//       user.age >= age &&
+//       user.friends.includes(friend);
+//     if (isTrue) {
+//       acc.push(user.name);
+//     }
+//     return acc;
+//   }, []);
+// };
+// console.log(getNamesInactiveUsersWithFriend(users, 20, "Jordan Sampson"));
